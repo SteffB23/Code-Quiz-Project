0f76e7ecaedfa5a4947fbcc7a4bf1ed6768482eb
SteffB23 <sbrookens@hotmail.com>
2021-09-30 14:54:38 -0400

    added vars and event listeners to needed buttons for page nav & continuity

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,91 @@ $(".btn-dark").on("click", function () {
     $(".card").hide();
     console.log("user clicked start");
     //Get the first question
+
+    //Get the first question
+    $(".highScorePage").hide();
+    $(".final-page").hide();
+    $(".timer").show();
+    $(".timer").html("Time:60")
+    $(".highScore").html("View Highscores");
+    $(".question-display").show();
+    $("#button-display").show();
+    quizQuestion.run();
+    quizQuestion.questionNumber = 0;
+    quizQuestion.correctGuesses = 0;
+    quizQuestion.incorrectGuesses = 0;
+    quizQuestion.getQuestion();
+    document.getElementById('userInput').value = " ";
 })
+
+// ON CLICK FOR RESET BUTTON - RESETS GAME
+$(".btn-secondary").on("click", function () {
+    console.log("user clicked Restart");
+    $(".highScorePage").hide();
+    $(".final-page").hide();
+    $(".timer").show();
+    $(".timer").html("Time: 60")
+    $(".highScore").html("View Highscores");
+    $(".question-display").show();
+    $("#button-display").show();
+    quizQuestion.run();
+    quizQuestion.questionNumber = 0;
+    quizQuestion.correctGuesses = 0;
+    quizQuestion.incorrectGuesses = 0;
+    quizQuestion.getQuestion();
+})
+$("#submitInitials").on("click", function () {
+    console.log("user clicked submit initials for high scores");
+    $(".highScorePage").show();
+    quizQuestion.highScorePage();
+})
+$("#resetScores").on("click", function () {
+    console.log("user clicked reset high scores");
+    localStorage.clear();
+    $("#hsArray").hide();
+})
+$("#goBack").on("click", function () {
+    console.log("user clicked to return from high scores high scores");
+    clearInterval(quizQuestion.countDownTimer);
+    $(".question-display").hide();
+    $("#button-display").hide();
+    $(".highScorePage").hide();
+    $(".card").show();
+    $(".timer").show();
+    $(".timer").html("Time: 60");
+    $(".highScore").show();
+    $("#hsArray").empty();
+})
+//Determine high score to be replaced
+$(".highScore").on("click", function () {
+    console.log("user clicked highScore");
+    quizQuestion.counter = 0;
+    quizQuestion.highScorePage();
+})
+// ON CLICK FOR ANSWER BUTTONS
+$("#button-display").on("click", ".answerButton", function (e) {
+    // answerButton.clicked(e); 
+    var selectedAnswer = $(e.target).attr("data-name");
+    console.log(e);
+    console.log(e.target);
+    console.log(e.target.data);
+    console.log($(e.target).attr("data-name"));
+    quizQuestion.checkAnswer(selectedAnswer);
+})
+// Global Variables
+var Counter = 0;
+var hrLine = document.createElement("hr");
+var highScore = 0;
+var quizQuestion = {
+    // current question
+    currentQuestion: "",
+    // correct answers 
+    correctGuesses: 0,
+    // incorrect answers 
+    incorrectGuesses: 0,
+    // counter 
+    counter: 0,
+    countDownTimer: null,
+    // question number 
+    questionNumber: 0,
+}
