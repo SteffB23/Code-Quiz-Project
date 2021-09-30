a3d78423b1f39de0202db0c3feb95f244d170748
SteffB23 <sbrookens@hotmail.com>
2021-09-30 14:44:59 -0400

    added functionality to start button to go the first question

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// ON CLICK EVENT FOR START BUTTON TO LOAD A QUESTION AND HIDE THE START BUTTON 
+$(".btn-dark").on("click", function () {
+    // remove homepage from view 
+    $(".card").hide();
+    console.log("user clicked start");
+    //Get the first question
+})
