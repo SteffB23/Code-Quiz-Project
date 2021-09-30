3d269592dec109888a9704bbca4f14e32de6a075
SteffB23 <sbrookens@hotmail.com>
2021-09-30 18:25:16 -0400

    added pages with basic question object with an array of questions from W3schools

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,4 +91,136 @@ var quizQuestion = {
     countDownTimer: null,
     // question number 
     questionNumber: 0,
+
+    // QUESTIONS OBJECT WHICH INCLUDES AN ARRAY OF QUESTIONS
+    // Questions from https://www.w3schools.com/quiztest/quiztest.asp?qtest=JavaScript
+    questions: [
+        {
+            // question text
+            questionText: "Inside which HTML element do we put the JavaScript?",
+            // question answers array
+            questionAnswer: ["<scripting>", "<script>", "<js>", "<javascript>"],
+            // correct answer 
+            answer: "<script>"
+        },
+        {
+            questionText: "Where is the correct place to insert a JavaScript?",
+            questionAnswer: ["Both in the <head> section and the <body> section are correct", "The <head> section", "The <body> section", "The <javascript> section"],
+            answer: "Both in the <head> section and the <body> section are correct"
+        },
+        {
+            questionText: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+            questionAnswer: ['<script name = "xxx.js">', '<script src = "xxx.js">', '<script href = "xxx.js">', '<script call = "xxx.js">'],
+            answer: '<script src = "xxx.js">',
+        },
+        {
+            questionText: 'How do you write "Hello World" in an alert box?',
+            questionAnswer: ['msg("Hello World");', 'msgBox("Hello World");', 'alert("Hello World");', 'alertBox("Hello World");'],
+            answer: 'alert("Hello World");',
+        },
+        {
+            questionText: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
+            questionAnswer: ["if(i <> 5)", "if i <> 5", "if i =! 5 then", "if (i != 5)"],
+            answer: "if (i != 5)",
+        },
+       
+    ],
+    run: function () {
+        clearInterval(this.countDownTimer);
+        this.countDownTimer = setInterval(this.decrement, 1000);
+        quizQuestion.counter = 60;
+    },
+
+    decrement: function () {
+        quizQuestion.counter--;
+        $(".timer").html("Time: " + quizQuestion.counter);
+        if (quizQuestion.counter <= 0) {
+            quizQuestion.counter = 0;
+            clearInterval(quizQuestion.countDownTimer);
+            quizQuestion.finalPage();
+            
+            //$("#initials").html("Sorry!  You timed out.")
+            $(".question-display").hide();
+            $("#button-display").hide();
+        }
+    },
+
+    // GET QUESTION METHOD
+    getQuestion: function () {
+        
+        // clear and hide a bunch of things when the question loads
+        $(".question-display").empty();
+        $(".areYouRight").empty();
+        $(".ready").empty();
+        // display question 
+        $(".question-display").html("<p>" + this.questions[this.questionNumber].questionText + "</p>");
+        this.buttonGenerator();
+    },
+
+    //BUTTON GENERATOR METHOD 
+    buttonGenerator: function () {
+        //empty buttons 
+        $("#button-display").empty();
+        // for loop to display answer buttons on the screen 
+        
+        for (var i = 0; i < this.questions[this.questionNumber].questionAnswer.length; i++) {
+            $("#button-display").append("<li>");
+            var a = $("<button>");
+            a.addClass("answerButton");
+            a.attr("data-name", this.questions[this.questionNumber].questionAnswer[i]);
+            a.text(this.questions[this.questionNumber].questionAnswer[i]);
+            //display button
+            $("#button-display").append(a);  
+            $("#button-display").append("</li>");          
+        };
+    },
+
+    // CHECK IF THE ANSWER IS CORRECT, WRONG, OR IF THE QUESTION TIMED OUT (UNDEFINED) 
+    checkAnswer: function (selectedAnswer) {
+        //determine if the answer is correct 
+        console.log(this.questions[this.questionNumber]);
+
+        if (selectedAnswer === this.questions[this.questionNumber].answer) {
+            console.log("win");
+            // increment the number correct 
+            this.correctGuesses++;
+            console.log(this.correctGuesses);
+            // display win message with teal hr
+            $(".areYouRight").html("<hr id='win'/>Correct!");
+            // next question 
+            this.questionNumber++;
+        }
+        else {
+            console.log("lose");
+            // increment incorrect guess 
+            this.incorrectGuesses++;
+            console.log(this.incorrectGuesses);
+            //Deduct 5 seconds for incorrect answer
+            quizQuestion.counter = quizQuestion.counter - 10;
+            // display lose message with red hr
+            $(".areYouRight").html("<hr id='lose'/> Wrong!");
+            // next question   
+            this.questionNumber++;
+        }
+        this.answerPage();
+    },
+
+    //DISPLAY ANSWER PAGE 
+    answerPage: function () {
+        // check for last question
+        setTimeout(function () {
+            if (quizQuestion.questionNumber < quizQuestion.questions.length) {
+                quizQuestion.getQuestion();
+            }
+            else {
+                quizQuestion.finalPage();
+            }
+        }, 1000
+        )
+    },
+
+    viewHighScore: function () {
+        $(".highScore").html("Highscore: " + highScore);
+    },
+
 }
